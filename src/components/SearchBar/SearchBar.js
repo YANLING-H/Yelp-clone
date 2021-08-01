f8b9cf5eddfe8c9be69d3a0e746453ff1b5f6a92
Yanling Hu <yanling.hu@mail.utoronto.ca>
2021-08-01 14:42:26 +0800

    add app, business, businessList component and abstruct business info from Business component to app component

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './SearchBar.css';
+
+const sortByOptions = {
+        'Best Match':'best_match',
+        'Highest Rated': 'rating',
+        'Most Reviewed': 'review_count'
+};
+
+class SearchBar extends React.Component {
+    
+       renderSortedByOptions(){
+           return Object.keys(sortByOptions).map(sortByOption => {
+               let sortByOptionValue = sortByOptions[sortByOption];
+               return <li key={sortByOptionValue}>{sortByOption}</li>;
+           });
+       }
+
+       render(){
+           return (
+            <div className="SearchBar">
+                <div className="SearchBar-sort-options">
+                <ul>
+                    {this.renderSortedByOptions()}
+                </ul>
+                </div>
+                <div className="SearchBar-fields">
+                    <input placeholder="Search Businesses" />
+                    <input placeholder="Where?" />
+                </div>
+                <div className="SearchBar-submit">
+                    <a href='www.#.com'>Let's Go</a>
+                </div>
+            </div>
+           )
+       }
+}
+
+export default SearchBar;
